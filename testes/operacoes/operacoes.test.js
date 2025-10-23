2a991a8fd90ccef96c27dbf196d19d46d49e62ea
leandrorodca <leandrorodca@gmail.com>
2025-10-23 19:41:02 -0300

    segundo commit

--- testes/operacoes/operacoes.test.js
+++ testes/operacoes/operacoes.test.js
@@ -1,10 +1,12 @@
 const sum = require('../../src/operacoes/sum');
 const sub = require('../../src/operacoes/sub');
 
-test('Soma dois números corretamente', () => {
+test('Soma de 1 + 2 = 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
-
+test('Soma de -1 + 2 = 1', () => {
+    expect(sum(-1, 2)).toBe(1);
+});
 test('Lança erro ao passar parâmetros não numéricos', () => {
     expect(() => sum(1, 'a')).toThrow('Os parâmetros devem ser números');
 });
